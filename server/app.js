574ed988092c770010b3a70f3f80484006173b05
Fabien Dubin <fabien.dubin@gmail.com>
2024-12-03 17:46:57 +0100

    Job done day 2

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
-const cohorts = require("./cohorts.json");
-const students = require("./students.json");
-const PORT = 5005;
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+// const cohorts = require("./cohorts.json");
+// const students = require("./students.json");
+const PORT = 5005;
+const Cohort = require("./models/Cohort.model.js");
+const Student = require("./models/Student.model.js");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -23,6 +27,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors({ origin: ["http://localhost:5173"] }));
 
+//MONGOOSE - Set up
+mongoose
+  .connect("mongodb://localhost:27017/cohort-tools-api")
+  .then((x) => console.log(`Connected to DB : ${x.connections[0].name} `))
+  .catch((err) => console.error("Error connection to DB", err));
+
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
 // ...
@@ -31,11 +41,27 @@ app.get("/docs", (req, res) => {
 });
 
 app.get("/api/cohorts", (req, res) => {
-  res.json(cohorts);
+  Cohort.find({})
+    .then((cohorts) => {
+      console.log("Retrieved cohorts", cohorts);
+      res.json(cohorts);
+    })
+    .catch((error) => {
+      console.log("error during retrieving cohorts", error);
+      res.status(500);
+    });
 });
 
 app.get("/api/students", (req, res) => {
-  res.json(students);
+  Student.find({})
+    .then((students) => {
+      console.log("Retrieved students", students);
+      res.json(students);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+    });
 });
 
 // START SERVER
