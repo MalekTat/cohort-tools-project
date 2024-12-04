1fcc53e1d3c8ff2b6032618386f625d4823cd446
Baptiste Desharbes <desharbesbaptiste@hotmail.fr>
2024-12-04 17:55:00 +0100

    all done for the day 3

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(cors({ origin: ["http://localhost:5173"] }));
 
 //MONGOOSE - Set up
 mongoose
-  .connect("mongodb://localhost:27017/cohort-tools-api")
+  .connect("mongodb://localhost:27017/cohort-tool-api")
   .then((x) => console.log(`Connected to DB : ${x.connections[0].name} `))
   .catch((err) => console.error("Error connection to DB", err));
 
@@ -40,11 +40,34 @@ app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
 
+app.post("/api/cohorts", (req, res) => {
+  Cohort.create({
+    cohortSlug: req.body.cohortSlug,
+    cohortName: req.body.cohortName,
+    program: req.body.program,
+    format: req.body.format,
+    campus: req.body.campus,
+    startDate: req.body.startDate,
+    inProgress: req.body.inProgress,
+    programManager: req.body.programManager,
+    leadTeacher: req.body.leadTeacher,
+    totalHours: req.params.totalHours,
+  })
+    .then((newCohort) => {
+      console.log("add new cohort", newCohort);
+      res.status(200).json(newCohort);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+    });
+});
+
 app.get("/api/cohorts", (req, res) => {
   Cohort.find({})
     .then((cohorts) => {
       console.log("Retrieved cohorts", cohorts);
-      res.json(cohorts);
+      res.status(200).json(cohorts);
     })
     .catch((error) => {
       console.log("error during retrieving cohorts", error);
@@ -52,6 +75,43 @@ app.get("/api/cohorts", (req, res) => {
     });
 });
 
+app.get("/api/cohorts/:cohortId", (req, res) => {
+  Cohort.findById(req.params.cohortId)
+    .then((oneCohort) => {
+      console.log("Retrived one cohort", oneCohort);
+      res.status(200).json(oneCohort);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+    });
+});
+
+app.put("/api/cohorts/:cohortId", (req, res) => {
+  Cohort.findByIdAndUpdate(req.params.cohortId, req.body, { new: true })
+    .then((oneCohort) => {
+      console.log("update one cohort", oneCohort);
+      res.status(200).json(oneCohort);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+    });
+});
+
+app.delete("/api/cohorts/:cohortId", (req, res) => {
+  Cohort.findByIdAndDelete(req.params.cohortId)
+    .then((oneCohort) => {
+      console.log("deleting one Cohort", oneCohort);
+      res.status(200).json(oneCohort);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+    });
+});
+
+// Returns all the students in JSON format
 app.get("/api/students", (req, res) => {
   Student.find({})
     .then((students) => {
@@ -61,6 +121,85 @@ app.get("/api/students", (req, res) => {
     .catch((err) => {
       console.log(err);
       res.status(500);
+    });
+});
+
+//Returns all the students of a specified cohort in JSON format
+
+app.get("/api/students/cohort/:cohortId", (req, res) => {
+  const { cohortId } = req.params;
+  Student.find({ cohort: cohortId })
+    .then((students) => {
+      console.log("Retrieved students for this cohort", students);
+      res.json(students);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+    });
+});
+
+//Returns the specified student by id
+app.get("/api/students/:studentId", (req, res) => {
+  Student.findById(req.params.studentId)
+    .then((oneStudent) => {
+      console.log("One student found", oneStudent);
+      res.json(oneStudent);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "failed to find the student" });
+    });
+});
+
+//Creates a new student with their respective cohort id
+app.post("/api/students", (req, res) => {
+  Student.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    phone: req.body.phone,
+    linkedinUrl: req.body.linkedinUrl,
+    languages: req.body.languages,
+    program: req.body.program,
+    background: req.body.background,
+    image: req.body.image,
+    projects: req.body.projects,
+    cohort: req.body.cohort, ///// EST CE QU'IL NE FAUDRAIT PAS METTRE L'ID
+  })
+    .then((newStudent) => {
+      console.log("add new student", newStudent);
+      res.status(200).json(newStudent);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500);
+    });
+});
+
+//Updates the specified student by id
+app.put("/api/students/:studentId", (req, res) => {
+  Student.findByIdAndUpdate(req.params.studentId, req.body, { new: true })
+    .then((oneStudent) => {
+      console.log("Student updated", oneStudent);
+      res.status(200).json(oneStudent);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "failed to update the student" });
+    });
+});
+
+//Deletes the specified cohort by id
+app.delete("/api/students/:studentId", (req, res) => {
+  Student.findByIdAndDelete(req.params.studentId)
+    .then((oneStudent) => {
+      console.log(" student deleted", oneStudent);
+      res.status(204).json(oneStudent);
+    })
+    .catch((err) => {
+      console.log("Oups", err);
+      res.status(500).json({ error: "Failed to retrieve the student" });
     });
 });
 
